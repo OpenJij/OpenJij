fbe6e7638b207d1c9bca0a3e79800b072995b568
yu yamashiro <yuyamashiro89@gmail.com>
2019-02-01 01:28:56 +0900

    add doxygen

---
+++ docs/html/search/all_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['response',['Response',['../classopenjij_1_1sampler_1_1_response.html',1,'openjij::sampler']]],
+  ['results',['Results',['../structopenjij_1_1sampler_1_1_results.html',1,'openjij::sampler']]]
+];
---
+++ docs/html/search/classes_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['updater',['Updater',['../structopenjij_1_1updater_1_1_updater.html',1,'openjij::updater']]]
+];
