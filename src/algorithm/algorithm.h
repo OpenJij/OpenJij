f8291305bec973d005e9ccd66732a1c184116139
j-i-k-o <jiko.rogy@gmail.com>
2019-03-26 22:47:04 +0900

    add cstddef header

--- src/algorithm/algorithm.h
+++ src/algorithm/algorithm.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <cstddef>
 
 namespace openjij {
 	namespace algorithm {
--- src/method/method.h
+++ src/method/method.h
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include"../graph/graph.h"
+#include <cstddef>
 
 namespace openjij {
 	namespace method {
