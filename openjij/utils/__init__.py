f500b1b152756c74d765608ba3b81caaf8dbc851
Masahito Kumagai <55102763+kumagaimasahito@users.noreply.github.com>
2019-09-20 15:40:18 +0900

    update sample_mean / standard_error function

--- openjij/utils/__init__.py
+++ openjij/utils/__init__.py
@@ -1,3 +1,2 @@
-from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability
+from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability, sample_mean, standard_error
 from .res_convertor import convert_response
-from .standard_error import standard_error
