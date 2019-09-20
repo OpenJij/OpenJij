3506259226193061cbf1c05d5e624a66382956a1
Masahito Kumagai <55102763+kumagaimasahito@users.noreply.github.com>
2019-09-20 19:38:35 +0900

    fix se func

--- openjij/utils/__init__.py
+++ openjij/utils/__init__.py
@@ -1,2 +1,2 @@
-from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability, sample_mean, standard_error
+from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability, se_residual_energy, se_success_probability
 from .res_convertor import convert_response
