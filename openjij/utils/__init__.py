3fbad2fae252f7c65026fe33aed729862d8b07c2
Masahito Kumagai <55102763+kumagaimasahito@users.noreply.github.com>
2019-09-26 20:43:27 +0900

    update se tts

--- openjij/utils/__init__.py
+++ openjij/utils/__init__.py
@@ -1,2 +1,2 @@
-from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability, se_residual_energy, se_success_probability
+from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability, se_residual_energy, se_success_probability, se_lower_tts, se_upper_tts
 from .res_convertor import convert_response
