2d8a3f164699aa91fda260c01ad88944e413c85a
yu yamashiro <yuyamashiro89@gmail.com>
2020-01-22 18:03:39 +0900

    bug fix

--- openjij/sampler/gpu_sa_sampler.py
+++ openjij/sampler/gpu_sa_sampler.py
@@ -16,10 +16,12 @@ import cxxjij
 import openjij
 from openjij.sampler import SASampler
 from openjij.model import BinaryQuadraticModel, ChimeraModel
+from openjij.graph_utils import chimera_to_ind
 import numpy as np
+import dimod
 
 
-class GPUSASampler(SASampler):
+class GPUSASampler(SASampler, dimod.Structured):
     """Sampler with Simulated Annealing (SA) on GPU.
 
     Inherits from :class:`openjij.sampler.sampler.BaseSampler`.
@@ -69,6 +71,14 @@ class GPUSASampler(SASampler):
 
         AttributeError: If GPU doesn't work.
 
+    Raises:
+        ValueError: [description]
+        AttributeError: [description]
+        ValueError: [description]
+        ValueError: [description]
+
+    Returns:
+        [type]: [description]
     """
 
     def __init__(self,
@@ -81,15 +91,56 @@ class GPUSASampler(SASampler):
         super().__init__(beta_min, beta_max, num_reads=num_reads,
                          num_sweeps=num_sweeps, schedule=schedule)
 
+    @property
+    def nodelist(self):
+        if 'unit_num_L' not in dir(self) or self.unit_num_L is None:
+            raise ValueError("this object is not set unit_num_L yet.")
+
+        return list(range(0, 8*8*self.unit_num_L*self.unit_num_L))
+
+    @property
+    def edgelist(self):
+        if 'unit_num_L' not in dir(self) or self.unit_num_L is None:
+            raise ValueError("this object is not set unit_num_L yet.")
+
+        edges = []
+        for r in range(self.unit_num_L):
+            for c in range(self.unit_num_L):
+                for z in range(8):
+                    i = chimera_to_ind(r, c, z, self.unit_num_L)
+                    if z in [0, 1, 2, 3]:
+                        edges += [
+                            (i, chimera_to_ind(r, c, _z, self.unit_num_L)
+                            for _z in [4, 5, 6, 7]
+                        ]
+                    else:
+                        edges += [
+                            (i, chimera_to_ind(r, c, _z, self.unit_num_L)
+                            for _z in [4, 5, 6, 7]
+                        ]
+                    edges.append(
+                        (i, chimera_to_ind(r+1, c, _z, self.unit_num_L)))
+                    edges.append(
+                        (i, chimera_to_ind(r, c+1, _z, self.unit_num_L)))
+                    if r != 0:
+                        edges.append(
+                            (i, chimera_to_ind(r-1, c, _z, self.unit_num_L)))
+                    if c != 0:
+                        edges.append(
+                            (i, chimera_to_ind(r, c-1, _z, self.unit_num_L)))
+
+        return edges
+
+
     def _dict_to_model(self, var_type, h=None, J=None, Q=None, **kwargs):
 
         if 'unit_num_L' in kwargs:
-            self.unit_num_L = kwargs['unit_num_L']
+            self.unit_num_L=kwargs['unit_num_L']
         elif not self.unit_num_L:
             raise ValueError(
                 'Input "unit_num_L" to the argument or the constructor of GPUSASampler.')
 
-        chimera = openjij.ChimeraModel(h=None, J=None, Q={(
+        chimera=openjij.ChimeraModel(h=None, J=None, Q={(
             (0, 0, 1), (0, 0, 4)): -1}, var_type=openjij.BINARY, unit_num_L=2, gpu=True)
 
         return chimera
@@ -100,7 +151,7 @@ class GPUSASampler(SASampler):
                  **kwargs):
         # Check the system for GPU is compiled
         try:
-            self.system_class = cxxjij.system.ChimeraClassicalGPU
+            self.system_class=cxxjij.system.ChimeraClassicalGPU
         except AttributeError:
             raise AttributeError(
                 'Does the computer you are running have a GPU? Compilation for the GPU has not been done. Please reinstall or compile.')
@@ -108,44 +159,44 @@ class GPUSASampler(SASampler):
         # convert to ChimeraModel from normal BQM
         if isinstance(model, BinaryQuadraticModel):
             if 'unit_num_L' in kwargs:
-                self.unit_num_L = kwargs['unit_num_L']
+                self.unit_num_L=kwargs['unit_num_L']
             elif not self.unit_num_L:
                 raise ValueError(
                     'Input "unit_num_L" to the argument or the constructor of GPUSASampler.')
-            chimera_model = ChimeraModel(
+            chimera_model=ChimeraModel(
                 model=model, unit_num_L=self.unit_num_L, gpu=True)
         else:
-            chimera_model = model
+            chimera_model=model
 
         if chimera_model.unit_num_L % 2 != 0:
             raise ValueError('unit_num_L should be even number.')
 
-        self.unit_num_L = chimera_model.unit_num_L
+        self.unit_num_L=chimera_model.unit_num_L
         self._set_model(chimera_model)
 
-        chimera = self.model.get_chimera_graph()
+        chimera=self.model.get_chimera_graph()
 
         # use all spins ?
-        self._use_all = len(model.indices) == (
+        self._use_all=len(model.indices) == (
             self.unit_num_L * self.unit_num_L * 8)
 
         if initial_state is None:
             def init_generator(): return chimera.gen_spin()
         else:
             if model.var_type == openjij.SPIN:
-                _init_state = np.array(initial_state)
+                _init_state=np.array(initial_state)
             else:  # BINARY
-                _init_state = (2*np.array(initial_state)-1).astype(int)
+                _init_state=(2*np.array(initial_state)-1).astype(int)
 
             def init_generator(): return _init_state
 
-        algorithm = cxxjij.algorithm.Algorithm_GPU_run
+        algorithm=cxxjij.algorithm.Algorithm_GPU_run
 
-        sa_system = cxxjij.system.make_chimera_classical_gpu(
+        sa_system=cxxjij.system.make_chimera_classical_gpu(
             init_generator(), chimera
         )
 
-        response = self._sampling(
+        response=self._sampling(
             chimera_model, init_generator,
             algorithm, sa_system, initial_state,
             reinitialize_state, seed, **kwargs
@@ -159,18 +210,18 @@ class GPUSASampler(SASampler):
     def _post_save(self, result_state, system, model, response):
         if not self._use_all:
             if model.coordinate == 'chimera coordinate':
-                indices = [model.to_index(
+                indices=[model.to_index(
                     x, y, z, model.unit_num_L) for x, y, z in model.indices]
             else:
-                indices = model.indices
-            result_state = np.array(result_state)[indices]
+                indices=model.indices
+            result_state=np.array(result_state)[indices]
         response.states.append(result_state)
         response.energies.append(model.calc_energy(
             result_state,
             need_to_convert_from_spin=True))
 
     def _set_model(self, model):
-        self.model = model
-        self.indices = model.indices
-        self.energy_bias = model.energy_bias
-        self.var_type = model.var_type
+        self.model=model
+        self.indices=model.indices
+        self.energy_bias=model.energy_bias
+        self.var_type=model.var_type
