d8b47123016477813d789bb6c7d0ea161dbdfc8f
j-i-k-o <jiko.rogy@gmail.com>
2020-03-27 17:06:58 +0900

    fix eigen fetch path

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -3,7 +3,8 @@ include(FetchContent)
 #### Eigen ####
 FetchContent_Declare(
     eigen
-    URL http://bitbucket.org/eigen/eigen/get/3.3.7.tar.gz
+    GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
+    GIT_TAG         3.3.7
     )
 
 FetchContent_GetProperties(eigen)
