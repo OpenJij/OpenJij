54e8e638a4dd62c58f9068cc6f7dca3b97df69d2
j-i-k-o <jiko.rogy@gmail.com>
2020-05-19 15:43:20 +0900

    fix HUBO

--- openjij/model/higher_order_model.py
+++ openjij/model/higher_order_model.py
@@ -3,12 +3,13 @@ import numpy as np
 
 class BinaryHigherOrderModel:
     """Higher order model.
-    Examples::
+    Examples:
         
-￼       >>> h = {0: -1}
-￼       >>> J = {(0, 1): -1}
-￼       >>> K = {(0, 1, 2): 1}
-        >>> model = BinaryHigherOrderModel([h, J, K])
+        ::
+￼           >>> h = {0: -1}
+￼           >>> J = {(0, 1): -1}
+￼           >>> K = {(0, 1, 2): 1}
+            >>> model = BinaryHigherOrderModel([h, J, K])
 ￼
     """
 
