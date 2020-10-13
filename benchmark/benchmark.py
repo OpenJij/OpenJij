e484571b11563e70a7fc4de93467c459e625d7cc
j-i-k-o <jiko.rogy@gmail.com>
2020-10-14 05:47:57 +0900

    update benchmark.py

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -12,8 +12,8 @@ h, J = {}, {}
 
 for i in range(n-1):
     for j in range(i+1, n):
-        #if np.random.random() <= 0.3:
-        J[i, j] = np.random.uniform(-1, 1)
+        if np.random.random() <= 0.3:
+            J[i, j] = np.random.uniform(-1, 1)
 
 NUM_READS = 10
 
@@ -54,8 +54,8 @@ for step in steps_openjij:
     print("\telapsed_time:{0}".format(elapsed_time) + "[sec]")
 
 # Benchmark OpenJij SQA
-print('OpenJij SQA')
-sampler = oj.SQASampler(beta=10, trotter=6)
+print('OpenJij SQA Dense')
+sampler = oj.SQASampler(beta=10, trotter=8)
 openjij_sql_time = []
 openjij_sql_energy = []
 for step in steps_openjij:
@@ -64,8 +64,23 @@ for step in steps_openjij:
     response = sampler.sample_ising(h, J, num_sweeps=step, num_reads=NUM_READS)
     elapsed_time = time.time() - start
 
-    openjij_sparse_time.append(elapsed_time)
-    openjij_sparse_energy.append(np.mean(response.energies))
+    openjij_sql_time.append(elapsed_time)
+    openjij_sql_energy.append(np.mean(response.energies))
+
+    print("\telapsed_time:{0}".format(elapsed_time) + "[sec]")
+
+print('OpenJij SQA Sparse')
+sampler = oj.SQASampler(beta=10, trotter=8)
+openjij_sqs_time = []
+openjij_sqs_energy = []
+for step in steps_openjij:
+    print('num_sweeps:', step)
+    start = time.time()
+    response = sampler.sample_ising(h, J, num_sweeps=step, num_reads=NUM_READS, sparse=True)
+    elapsed_time = time.time() - start
+
+    openjij_sqs_time.append(elapsed_time)
+    openjij_sqs_energy.append(np.mean(response.energies))
 
     print("\telapsed_time:{0}".format(elapsed_time) + "[sec]")
 
@@ -90,6 +105,8 @@ for step in steps_openjij:
 import matplotlib.pyplot as plt
 plt.plot(openjij_time, openjij_energy, '-o', label='openjij-dense')
 plt.plot(openjij_sparse_time, openjij_sparse_energy, '-o', label='openjij-sparse')
+plt.plot(openjij_sql_time, openjij_sql_energy, '-o', label='openjij-sqa-dense')
+plt.plot(openjij_sqs_time, openjij_sqs_energy, '-o', label='openjij-sqa-sparse')
 plt.plot(neal_time, neal_energy, '-o', label='neal')
 plt.xlabel('time [sec]')
 plt.ylabel('Energy')
