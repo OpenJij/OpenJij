ec0b069ae15e611a797bee74a9b95fa47f1e9866
Yoshiki Sakamoto <iikuni29roukamakurabakuhu@gmail.com>
2021-04-16 15:59:37 +0900

    Update boost.cmake

--- external/boost.cmake
+++ external/boost.cmake
@@ -1,14 +1,7 @@
 include(FetchContent)
 FetchContent_Declare(
-    Boost
+    boostorg
     GIT_REPOSITORY https://github.com/boostorg/boost.git
-    GIT_SUBMODULES libs/system libs/serialization libs/random
-                   libs/function libs/config libs/headers libs/assert libs/core libs/integer
-                   libs/type_traits libs/mpl libs/throw_exception libs/preprocessor libs/utility
-                   libs/static_assert libs/smart_ptr libs/predef libs/move libs/io libs/iterator
-                   libs/detail libs/spirit libs/optional libs/type_index libs/container_hash
-                   libs/array libs/bind
-                   tools/build tools/boost_install
     GIT_TAG        boost-1.75.0
 )
 
