8b7992c26e1368c05b8650ca4832b251cb28cc9f
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-15 14:50:45 +0900

    Update googletest.cmake

--- external/googletest.cmake
+++ external/googletest.cmake
@@ -6,5 +6,6 @@ FetchContent_Declare(
     GIT_REPOSITORY  https://github.com/google/googletest.git
     GIT_TAG         release-1.11.0
 )
-
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
 FetchContent_MakeAvailable(googletest)
+
