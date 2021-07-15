02e742dfcea8b1c73b741d77cd74c18b01c094f3
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-16 00:02:28 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -6,15 +6,20 @@ FetchContent_Declare(
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.3.9
     )
-
+set(CMAKE_CXX_STANDARD 11) 
+add_definitions(-DEIGEN_MPL2_ONLY)
+if (APPLE)
+    if(BLAS_FOUND AND LAPACK_FOUND) 
+      add_definitions(-DEIGEN_USE_BLAS)
+      add_definitions(-DEIGEN_USE_LAPACKE)
+    endif()
+endif()
 FetchContent_MakeAvailable(eigen)
 
 add_library(eigen_lib INTERFACE)
 target_include_directories(eigen_lib INTERFACE ${eigen_SOURCE_DIR})
 target_compile_definitions(eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 if (APPLE)
-    find_package(BLAS REQUIRED)
-    find_package(LAPACK REQUIRED)
     if(BLAS_FOUND AND LAPACK_FOUND) 
       target_compile_definitions(eigen_lib INTERFACE EIGEN_USE_BLAS=ON)
       target_compile_definitions(eigen_lib INTERFACE EIGEN_USE_LAPACKE=ON)
