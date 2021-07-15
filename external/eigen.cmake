88c7e494c91b685705ac1fe45911661e1f316188
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-15 12:40:04 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -4,17 +4,19 @@ include(FetchContent)
 FetchContent_Declare(
     eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
-    GIT_TAG         3.3.7
+    GIT_TAG         3.3.9
     )
 
-FetchContent_GetProperties(eigen)
-
-if(NOT eigen_POPULATED)
-    message(STATUS "Fetch eigen")
-    FetchContent_Populate(eigen)
-    message(${eigen_SOURCE_DIR})
-    add_subdirectory(${eigen_SOURCE_DIR})
+set(EIGEN_MPL2_ONLY ON)
+if (APPLE)
+    find_package(BLAS REQUIRED)
+    find_package(LAPACK REQUIRED)
+    if(BLAS_FOUND AND LAPACK_FOUND) 
+      set(EIGEN_USE_BLAS ON) 
+      set(EIGEN_USE_LAPACKE ON)
+    endif()
 endif()
+FetchContent_MakeAvailable(eigen)
 
 add_library(eigen_lib INTERFACE)
 target_include_directories(eigen_lib INTERFACE ${eigen_SOURCE_DIR})
