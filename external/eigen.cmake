6b8a9a69e298716d94e7c9d1e5a6e36e3304aba1
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-15 14:48:13 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -7,15 +7,20 @@ FetchContent_Declare(
     GIT_TAG         3.3.9
     )
 
-set(EIGEN_MPL2_ONLY ON)
+FetchContent_MakeAvailable(eigen)
+
+add_library(eigen_lib INTERFACE)
+target_include_directories(eigen_lib INTERFACE ${eigen_SOURCE_DIR})
+target_compile_definitions(eigen_lib INTERFACE EIGEN_MPL2_ONLY)
 if (APPLE)
     find_package(BLAS REQUIRED)
     find_package(LAPACK REQUIRED)
     if(BLAS_FOUND AND LAPACK_FOUND) 
-      set(EIGEN_USE_BLAS ON) 
-      set(EIGEN_USE_LAPACKE ON)
+      target_compile_definitions(eigen_lib INTERFACE EIGEN_USE_BLAS=ON)
+      target_compile_definitions(eigen_lib EIGEN_USE_LAPACKE=ON)
     endif()
 endif()
-FetchContent_MakeAvailable(eigen)
+
+
 
 
