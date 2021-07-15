07db02c7c2e5253d8cdeeb84499efc2b99722de7
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-16 00:10:13 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -6,7 +6,7 @@ FetchContent_Declare(
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.3.9
     )
-set(CMAKE_CXX_STANDARD 11) 
+set(EIGEN_CPP_STANDARD -std=c++11)
 add_definitions(-DEIGEN_MPL2_ONLY)
 if (APPLE)
     if(BLAS_FOUND AND LAPACK_FOUND) 
