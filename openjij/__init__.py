87b2c866bb03d3ad97089c3e2f708740312b718c
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 07:52:30 +0900

    Update __init__.py

--- openjij/__init__.py
+++ openjij/__init__.py
@@ -1,8 +1,15 @@
-#from cxxjij import *
+try:
+    import typing 
+except ImportError:
+    from typing_extensions import * 
 import cxxjij
-from .__version import __version__
-from .variable_type import SPIN, BINARY, Vartype, cast_vartype
-from .sampler import Response
-from .sampler import SASampler, SQASampler, CSQASampler
-from .model import BinaryQuadraticModel, BinaryPolynomialModel
-from .utils import solver_benchmark, convert_response
+import openjij.model
+import openjij.sampler 
+import openjij.sampler.chimera_gpu
+import openjij.utils
+
+from openjij.variable_type import SPIN, BINARY, Vartype, cast_vartype
+from openjij.sampler import Response
+from openjij.sampler import SASampler, SQASampler, CSQASampler
+from openjij.model import BinaryQuadraticModel, BinaryPolynomialModel
+from ooenjij.utils import solver_benchmark, convert_response
