6d42a6d849f661bb8c5a1664a72ae51f9d0280ed
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2021-07-26 09:49:00 +0900

    Update eigen.cmake

--- external/eigen.cmake
+++ external/eigen.cmake
@@ -5,6 +5,7 @@ FetchContent_Declare(
     eigen
     GIT_REPOSITORY  https://gitlab.com/libeigen/eigen
     GIT_TAG         3.3.9
+    CMAKE_ARGS -DEIGEN_MPL2_ONLY
     )
 set(EIGEN_CPP_STANDARD -std=c++11)
 FetchContent_MakeAvailable(eigen)
