a1d8be088b43cbb6e8312f41a432f7e30f0bbeba
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-04-12 08:51:58 +0900

    Update pybind11-json.cmake

--- external/pybind11-json.cmake
+++ external/pybind11-json.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
     pybind11_json
     GIT_REPOSITORY  https://github.com/pybind/pybind11_json
-    GIT_TAG         0.2.11 
+    GIT_TAG         0.2.12
 )
 
 FetchContent_MakeAvailable(pybind11_json)
