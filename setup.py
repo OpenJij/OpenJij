e6c8a1cc772a6e76f2692efb8b6c9d18ce37ee80
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-05-23 12:15:42 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -155,10 +155,10 @@ class GoogleTestCommand(TestCommand):
         print("\nPython tests complete, now running C++ tests...\n")
         # Run catch tests
         print(os.path.join('build/', self.distutils_dir_name('lib')))
-        subprocess.call(['make cxxjij_test'],
-                        cwd=os.path.join('build',
-                                         self.distutils_dir_name('temp')),
-                        shell=True)
+        #subprocess.call(['cxxjij_test'],
+        #                cwd=os.path.join('build',
+        #                                 self.distutils_dir_name('temp')),
+        #                shell=True)
         subprocess.call(['./tests/cxxjij_test'],
                         cwd=os.path.join('build',
                                          self.distutils_dir_name('temp')),
