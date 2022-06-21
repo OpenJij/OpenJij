af5b3a4b5cd41e14f65fca46e0c4d198264c973d
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-22 08:06:40 +0900

    Update gpu_sqa_sampler.py

--- openjij/sampler/chimera_gpu/gpu_sqa_sampler.py
+++ openjij/sampler/chimera_gpu/gpu_sqa_sampler.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import cxxjij
+import openjij.cxxjij as cxxjij
 import openjij
 from openjij.sampler import SQASampler
 from .base_gpu_chimera import BaseGPUChimeraSampler
