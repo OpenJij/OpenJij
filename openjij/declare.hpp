598ccd4510920293d705d2334ef8ba7526f6b65e
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-22 07:23:27 +0900

    Update declare.hpp

--- openjij/declare.hpp
+++ openjij/declare.hpp
@@ -12,8 +12,7 @@
 //    See the License for the specific language governing permissions and
 //    limitations under the License.
 
-#ifndef OPENJIJ_DECLARE_HPP__
-#define OPENJIJ_DECLARE_HPP__
+#pragma once
 
 #include <graph/all.hpp>
 #include <system/all.hpp>
