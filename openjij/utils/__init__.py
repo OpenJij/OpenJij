c9aeaf0b571ab90a96fb0c19a862a7f30dffbfe2
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-22 08:09:16 +0900

    Update __init__.py

--- openjij/utils/__init__.py
+++ openjij/utils/__init__.py
@@ -1,3 +1,7 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
 from .benchmark import solver_benchmark, residual_energy, time_to_solution, success_probability, se_residual_energy, se_success_probability, se_upper_tts, se_lower_tts
 from .res_convertor import convert_response
 import openjij.utils.graph_utils
