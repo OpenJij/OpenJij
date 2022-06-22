ba8d04e567279d8fb48d1340675f7dc57c070957
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-22 13:52:17 +0900

    Update __init__.py

--- openjij/__init__.py
+++ openjij/__init__.py
@@ -6,17 +6,15 @@ try:
     import typing 
 except ImportError:
     from typing_extensions import * 
-import openjij.cxxjij
-import openjij.model
-import openjij.sampler 
-import openjij.sampler.chimera_gpu
-import openjij.utils
 
 from openjij.variable_type import SPIN, BINARY, Vartype, cast_vartype
-from openjij.sampler import Response
-from openjij.sampler import SASampler, SQASampler, CSQASampler
-from openjij.model import BinaryQuadraticModel, BinaryPolynomialModel
-from openjij.utils import solver_benchmark, convert_response
+from openjij.sampler.response import Response
+from openjij.sampler.csqa_sampler import CSQASampler
+from openjij.sampler.sa_sampler import SASampler
+from openjij.sampler.sqa_sampler import SQASampler
+from openjij.model.model import BinaryQuadraticModel, BinaryPolynomialModel
+from openjij.utils.benchmark import solver_benchmark
+from openjij.utils.res_convertor import convert_response
 
 __all__ = [ 
     "SPIN", 
