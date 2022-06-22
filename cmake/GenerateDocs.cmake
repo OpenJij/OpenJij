32c51045de4d5ab1818acda448f9327c288a513a
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-22 16:00:33 +0900

    Update GenerateDocs.cmake

--- cmake/GenerateDocs.cmake
+++ cmake/GenerateDocs.cmake
@@ -106,6 +106,10 @@ if (DOXYGEN_FOUND)
         set(DOXYGEN_REFERENCES_RELATION YES)
         
         #set(DOXYGEN_UML_LIMIT_NUM_FIELDS 100)
+        
+        set(INPUT "${PROJECT_SOURCE_DIR}/src/" "${PROJECT_SOURCE_DIR}/openjij/" "${PROJECT_SOURCE_DIR}/README.md")
+        set(USE_MDFILE_AS_MAINPAGE "README.md")
+        
         doxygen_add_docs(cxxjij_header_only_docs
                          ${PROJECT_SOURCE_DIR}/src
                          ALL
