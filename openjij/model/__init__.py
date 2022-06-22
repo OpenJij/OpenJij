e3188d5dd3fa6443937eb3041cd82b0667d0f8c2
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-22 13:34:29 +0900

    Update __init__.py

--- openjij/model/__init__.py
+++ openjij/model/__init__.py
@@ -2,4 +2,18 @@ from pkgutil import extend_path
 
 __path__ = extend_path(__path__, __name__)
 
-from .model import *
+import openjij.model.chimera_model 
+import openjij.model.king_graph
+from openjij.model.model import make_BinaryQuadraticModel, make_BinaryQuadraticModel_from_JSON, BinaryQuadraticModel, bqm_from_numpy_matrix, make_BinaryPolynomialModel, make_BinaryPolynomialModel_from_JSON, BinaryPolynomialModel, make_BinaryPolynomialModel_from_hising, make_BinaryPolynomialModel_from_hubo
+
+__all__ = [ 
+  "make_BinaryQuadraticModel", 
+  "make_BinaryQuadraticModel", 
+  "BinaryQuadraticModel", 
+  "bqm_from_numpy_matrix", 
+  "make_BinaryPolynomialModel", 
+  "make_BinaryPolynomialModel_from_JSON", 
+  "BinaryPolynomialModel", 
+  "make_BinaryPolynomialModel_from_hising", 
+  "make_BinaryPolynomialModel_from_hubo",
+]
