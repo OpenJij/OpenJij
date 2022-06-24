73701f222ec9e71b67e03f54ce9e665d67ed2a66
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-06-24 21:53:41 +0900

    Update gpu_sa_sampler.py

--- openjij/sampler/chimera_gpu/gpu_sa_sampler.py
+++ openjij/sampler/chimera_gpu/gpu_sa_sampler.py
@@ -104,7 +104,7 @@ class GPUChimeraSASampler(SASampler, BaseGPUChimeraSampler):
 
         Examples::
 
-            >>> sampler = openjij.GPUChimeraSASampler(unit_num_L=2)
+            >>> sampler = openjij.sampler.chimera_gpu.gpu_sa_sampler.GPUChimeraSASampler(unit_num_L=2)
             >>> h = {0: -1, 1: -1, 2: 1, 3: 1},
             >>> J = {(0, 4): -1, (2, 5): -1}
             >>> res = sampler.sample_ising(h, J)
