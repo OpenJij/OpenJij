a28775b199e0f106edf64d3fed5169b303f8ff65
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-07-08 15:09:48 +0900

    Update cimod.cmake

--- external/cimod.cmake
+++ external/cimod.cmake
@@ -24,7 +24,7 @@ set(FETCHCONTENT_QUIET OFF)
 FetchContent_Declare(
     cimod
     GIT_REPOSITORY  https://github.com/OpenJij/cimod
-    GIT_TAG         v1.4.13
+    GIT_TAG         v1.4.14
     GIT_SHALLOW     TRUE
     )
 
