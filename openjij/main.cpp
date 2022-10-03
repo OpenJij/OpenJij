1597ae6ede47709f5f896ef7b017457f469126fd
Yoshiki Sakamoto <y.sakamoto@j-ij.com>
2022-10-03 17:12:06 +0900

    Update main.cpp

--- openjij/main.cpp
+++ openjij/main.cpp
@@ -26,9 +26,12 @@
 
 PYBIND11_MODULE(cxxjij, m) {
   py::options options;
-  options.disable_function_signatures();
+  #options.disable_function_signatures();
   
   m.doc() = "openjij is a framework for ising and qubo";
+  
+  auto py_size_t = py::class_<std::size_t>(m, "size_t");
+  py_size_t.def(py::init<const std::size_t&>());
 
   /**********************************************************
    //namespace graph
